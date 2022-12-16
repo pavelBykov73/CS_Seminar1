526c8b9ea4c9ff79d178f4a65784c2e1ea70c9d9
PavelB <pavel.bykov73@yandex.ru>
2022-12-16 11:57:20 +0300

    write and debug exersize 1_1

--- ex1_1/Program.cs
+++ ex1_1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача №1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+a = 25, b = 5 -> да 
+a = 2, b = 10 -> нет 
+a = 9, b = -3 -> да 
+a = -3 b = 9 -> нет
+*/
+Console.Clear();
+Console.WriteLine("Введите первое число:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 == (Math.Pow(number2,2))){
+    Console.WriteLine($"Да, квадрат числа {number2} ({number2}^2) = числу {number1}");
+}
+else{
+    Console.WriteLine($"НЕТ, квадрат числа {number2} ({number2}^2) != числу {number1}");
+}
