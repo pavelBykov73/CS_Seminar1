2b0bbdab53773f7b4335558cc821d4b8bf944d9e
PavelB <pavel.bykov73@yandex.ru>
2022-12-20 16:01:27 +0300

    Add exersise 2_10

---
+++ ex2_10/ex2_10.cs
@@ -0,0 +1,19 @@
+﻿/*
+Семинар 2.
+ЗЗадача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+Console.Clear();
+Console.WriteLine("Введите трехзначное положительное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 999 || number <100){
+    Console.WriteLine("Допустимы числа от 100 до 999!");
+    Console.WriteLine("Перезапустите программу...");
+    return;
+}
+int digit2=(number/10)%10;
+Console.WriteLine($"Вторая цифра = {digit2}");
+
