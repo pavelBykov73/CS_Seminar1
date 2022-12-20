1bbf8e5ef373e87ff9b1dc99f308a60da3484ff2
PavelB <pavel.bykov73@yandex.ru>
2022-12-20 16:37:56 +0300

    Add exersise 2_15

---
+++ ex2_15/ex2_15.cs
@@ -0,0 +1,31 @@
+﻿/*
+Семинар 2.
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+Console.Clear();
+Console.WriteLine("Введите день недели числом (1-7):");
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number){
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        Console.WriteLine("Нет, рабочий день");
+    break;
+    
+    case 6:
+    case 7:
+        Console.WriteLine("Да, выходной");
+    break;
+
+    default:
+        Console.WriteLine("Что-то не то с числом!");
+        Console.WriteLine("Допустимы числа от 1 до 7!");
+        Console.WriteLine("Перезапустите программу...");
+    break;    
+}
