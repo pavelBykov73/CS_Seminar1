073dbd33ebd0d07c7f896506f06fe33a0b17d4f6
PavelB <pavel.bykov73@yandex.ru>
2022-12-26 16:46:06 +0300

    Add exersise 3_23

---
+++ ex3_23/ex3_23.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 23
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int power3(int value)
+{
+    return (int) Math.Pow(value, 3);
+}
+
+void printResult(int value)
+{
+    Console.WriteLine($"{value}^3 = {power3(value)}");
+}
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number >= 1){
+    for(int i = 1; i <= number; i++){
+        printResult(i);
+    }   
+}
+else{
+    for(int i = 1; i >= number; i--){
+        printResult(i);
+    }   
+}
