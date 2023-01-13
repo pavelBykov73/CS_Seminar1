95a9f61bcc69941c5ef0d0d00ea82beb9b4f47ee
PavelB <pavel.bykov73@yandex.ru>
2023-01-13 15:59:49 +0300

    Add exersise 4_25

---
+++ ex4_25/ex4_25.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+Console.Clear();
+Console.WriteLine("Введите число A:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите положительное число B:");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b < 0){
+    Console.WriteLine("Допустимы только положительные числа B!");
+    Console.WriteLine("Перезапустите программу...");
+    return;
+}
+int res = myPow(a,(uint)b);
+Console.WriteLine($"A^B = {res}");
+
+int myPow(int num, uint pow)
+{
+    int result=1;
+    for(int i=0; i<pow; i++){
+        result*=num;
+    }
+    return result;
+}
