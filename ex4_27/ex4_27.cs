bb7ed4f9f36e1df84332361497be2dca5606fbc1
PavelB <pavel.bykov73@yandex.ru>
2023-01-14 12:15:55 +0300

    Add exersise 4_27

---
+++ ex4_27/ex4_27.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Clear();
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int res = sumOfDigit(number);
+Console.WriteLine($"Sum of digits = {res}");
+
+int sumOfDigit(int num)
+{
+    num = Math.Abs(num);
+    int result=0;
+    while(num > 0){
+        result+=num%10;
+        num=num/10;
+    }
+    return result;
+}
